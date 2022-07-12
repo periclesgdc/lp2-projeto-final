b7044ce8a057b3f5adb0d63bc3e92d6dd23ab426
davi.goncalves <davigoncalves@movida.com.br>
2022-07-12 00:56:40 -0300

    removendo estoque.

--- src/main/java/com/edu/minimarket/connection/ORMEstoque.java
+++
@@ -1,8 +0,0 @@
-package com.edu.minimarket.connection;
-
-public class ORMEstoque extends ORM<Estoque>{
-    public ORMEstoque(Class<Estoque> classeExtendida) {
-        super(classeExtendida);
-    }
-
-}
